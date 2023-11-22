0ba5161d70ff62db551e0f9768e876943402e635
EdmondMuresan <147847495+EdmondMuresan@users.noreply.github.com>
2023-11-22 12:35:19 +0200

    mychanges

--- utilizatorimodificare.php
+++ utilizatorimodificare.php
@@ -28,36 +28,6 @@ if (!empty($_POST['id'])) {
         } else {
             echo "id incorect!";
         }
-categorie<?php
-include("Conectare.php");
-
-$error = '';
-
-if (!empty($_POST['id'])) {
-    if (isset($_POST['submit'])) {
-        if (is_numeric($_POST['id'])) {
-            $id = $_POST['id'];
-            $nume = htmlentities($_POST['nume'], ENT_QUOTES);
-            $prenume = htmlentities($_POST['prenume'], ENT_QUOTES);
-            $password = htmlentities($_POST['password'], ENT_QUOTES);
-            $username = htmlentities($_POST['username'], ENT_QUOTES);
-            $telefon = htmlentities($_POST['telefon'], ENT_QUOTES);
-            $email = htmlentities($_POST['email'], ENT_QUOTES);
-
-            if ($nume == '' || $prenume == '' || $password == '' || $username == '' || $telefon == '' || $email == '') {
-                echo "<div> ERROR: Completati campurile obligatorii!</div>";
-            } else {
-                if ($stmt = $mysqli->prepare("UPDATE utilizator SET nume=?, prenume=?, password=?, username=?, telefon=?, email=? WHERE id='".$id."'")) {
-                    $stmt->bind_param("ssssss", $nume, $prenume, $password, $username, $telefon, $email);
-                    $stmt->execute();
-                    $stmt->close();
-                } else {
-                    echo "ERROR: nu se poate executa update.";
-                }
-            }
-        } else {
-            echo "id incorect!";
-        }
     }
 }
 ?>
